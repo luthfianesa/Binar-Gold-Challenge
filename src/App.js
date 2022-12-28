c914e4a2509576eaf858c6915810d7c883299f05
luthfianesa <lutvheanesa@gmail.com>
2022-12-28 12:15:42 +0700

    Add Document Title

--- src/App.js
+++ src/App.js
@@ -2,11 +2,18 @@ import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage"
 import SearchCar from "./pages/SearchCar"
 import CarDetail from "./pages/CarDetail"
+import { useEffect } from "react";
 
 
 const App = () => {
+  const DocumentTitle = () => {
+    useEffect(() => {
+      document.title = "Binar Car Rent"
+    })
+  }
   return (
     <div>
+      <DocumentTitle />
       <Routes>
         <Route path="/" element={<HomePage/>}/>
         <Route path="/searchcar" element={<SearchCar/>}/>
